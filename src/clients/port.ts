dccb75258469ae5cceae7a7e9d38a210a2241574
Alexandro521 <elavion0151@gmail.com>
2026-02-25 21:21:51 -0400

    client register

--- src/clients/port.ts
+++ src/clients/port.ts
@@ -1,9 +1,26 @@
 import {MangaServerClient} from "./leerCapitulo.js"
-import type {MangaServerInterface} from "../types/types.js"
+import { MangaDex } from "./mangadex.js"
+import {MangaServerInterface, type ConfigurationClient} from "../types/types.js"
+import type { Page } from "playwright"
 
+interface Client extends ConfigurationClient {
+    client:  (e:Page) => MangaServerInterface
+}
+type RegisterInterface = {
+    [key in ConfigurationClient['name']]: Client
+}
 
-export const mangaServerRegister = new Map<string, typeof MangaServerInterface>([
-    ['leerCapitulo', MangaServerClient],
-])
+export const mangaServerRegister: RegisterInterface = {
+    'leercapitulo': {
+        name: 'leercapitulo',
+        need_browser: true,
+        client: (e: Page) => new MangaServerClient(e),
+    },
+    'mangadex': {
+        name: 'mangadex',
+        need_browser: false,
+        client: (e:Page) => new MangaDex()
+    }
+}
 
 
