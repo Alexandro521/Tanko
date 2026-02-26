b7f210694077bf9932501ef97cfd4aed414f4be8
Alexandro521 <elavion0151@gmail.com>
2026-02-25 23:59:32 -0400

    fix

--- src/functions/lang.ts
+++ src/functions/lang.ts
@@ -1,6 +1,6 @@
 import es from "../lang/es.json" with {type : "json"};
 import en from "../lang/en.json" with {type : "json"};
-import type { LangInterface } from "@/types/lang.js";
+import type { LangInterface } from "../types/lang.js";
 
 export const lang: {[key:string]: LangInterface} = {
     es,
