1c9f85c40c266e508d4423bac4921980af5a3ae6
Alexdev <elavion0151@gmail.com>
2026-04-05 15:54:11 -0400

    soporte para mangadex: actualizando loadMangaChapter

--- src/clients/mangadex.ts
+++ src/clients/mangadex.ts
@@ -36,18 +36,20 @@ export class MangaDex implements MangaServerInterface{
             );
 
             const chapterMap = new Map<string, number>()
+
             const chapterList: Chapter[] = []
 
             for(const chapterData of res.data.data){
                 const chapterNumber = chapterData.attributes.chapter ?? ''
                 const chapterLang = chapterData.attributes.translatedLanguage as ChapterLangKey
-
+                const title = `Chapter ${chapterData.attributes.chapter}${chapterData.attributes.title ? ': ' + chapterData.attributes.title : ''}`
                 if(chapterMap.has(chapterNumber)){
                     const target = chapterList[chapterMap.get(chapterNumber) ?? 0 ];
                     target.lang_n++
 
                     target.src[chapterLang] = {
                         lang: chapterLang,
+                        title,
                         src: chapterData.id
                     }
                     continue
@@ -56,19 +58,19 @@ export class MangaDex implements MangaServerInterface{
                 const chapterInfo:Chapter = {
                     id: chapterData.id,
                     index: chapterNumber,
-                    title: `Chapter ${chapterData.attributes.chapter}
-                    ${chapterData.attributes.title ? ': ' + chapterData.attributes.title : ''}`,
+                    title: `Chapter ${chapterData.attributes.chapter}${chapterData.attributes.title ? ': ' + chapterData.attributes.title : ''}`,
                     lang_n: 1,
                     src: {},
                 }
                 chapterInfo.src[chapterLang] = {
                     lang: chapterLang,
+                    title: `Chapter ${chapterData.attributes.chapter}${chapterData.attributes.title ? ': ' + chapterData.attributes.title : ''}`,
                     src: chapterData.id
                 }
                 chapterList.push(chapterInfo)
                 chapterMap.set(chapterNumber, chapterList.length -1)
             }
-
+            chapterList.sort((a,b) =>  Number(b.index ) - Number(a.index))
             return {
                 title: res.data.data[0].attributes.title ?? '',
                 chapters: chapterList 
