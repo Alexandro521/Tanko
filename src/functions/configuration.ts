d60fec1788090e4fd421db03e9e45033c89066dc
Alexandro521 <elavion0151@gmail.com>
2026-06-04 23:13:59 -0400

    Update configuration.ts

--- src/functions/configuration.ts
+++ src/functions/configuration.ts
@@ -7,68 +7,61 @@ import fsPromise from "fs/promises";
 import ora from "ora";
 import path from "path";
 import { type BrowserContext, firefox, type Page } from "playwright";
-import { z } from "zod";
 import { CONFIG_FILE_PATH, DOWNLOADS_DEFAULT_DIR, LAUNCH_OPTIONS, TEMP_DIR } from "../const.js";
 import { lang } from "./lang.js";
 import EventEmitter from "events";
-
-const schema = z.object({
-    deepSearch: z.boolean(),
-    downloads_path: z.string(),
-    language: z.string<'es' | 'en'>(),
-    client: z.object({
-        name: z.string<'mangadex' | 'leercapitulo'>(),
-        need_browser: z.boolean()
-    }
-    ),
-})
-
 const spin = ora()
-export class Configuration extends EventEmitter {
-    private static instace: Configuration | null = null
-    private context!: Page 
-    private browser: BrowserContext | null = null
-    private lang!: LangInterface
-    private client!: MangaServerInterface
-    private config: ConfigurationInterface = {
-        deepSearch: false,
-        downloads_path: DOWNLOADS_DEFAULT_DIR,
-        language: 'es',
-        client: mangaServerRegister['leercapitulo']
-    }
 
+export class Configuration extends EventEmitter {
+    private static confInstance: Configuration
+    private browser: BrowserContext | null = null
+    private client!: MangaServerInterface
+    private lang!: LangInterface
+    private context!: Page 
+  
+    private config: ConfigurationInterface = {
+        downloads_path: DOWNLOADS_DEFAULT_DIR,
+        deepSearch: false,
+        language: 'es',
+        client: mangaServerRegister.mangadex,
+        favoriteChapterLang: 'any',
+        historyMaxSize: 256,
+        historyServerFilter: true,
+        imageCacheMaxSize: '64'
+    }
+  
     private constructor() {
         super()
     }
 
     static async getInstance() {
-        if (!this.instace) {
-            this.instace = new Configuration()
-            await this.instace.load()
-            return this.instace
+        if (!this.confInstance) {
+            this.confInstance = new Configuration()
+            await this.confInstance.load()
+            return this.confInstance
         }
-        return this.instace
+        return this.confInstance
     }
+  
     get configuration(){
         return this.config
     }
     async loadBrowser() {
         try {
-            spin.start('loading Browser')
+            spin.start('Loading browser...')
             this.browser = await firefox.launchPersistentContext(
-                path.join(TEMP_DIR, 'tanko', 'browser'),
-                LAUNCH_OPTIONS);
-
+              path.join(TEMP_DIR, 'tanko', 'browser'), LAUNCH_OPTIONS);
             spin.stop()
             await this.browser.addInitScript(() => {
-                const storageData = [
+              /*for leercapitulo.co */
+              const storageData = [
                     { name: "display_mode", value: "1" },
                     { name: "pic_style", value: "0" }
-                ];
-                if (window.location.hostname.includes('leercapitulo.co')) {
-                    storageData.forEach(item => {
-                        window.localStorage.setItem(item.name, item.value);
-                    });
+              ];
+              if (window.location.hostname.includes('leercapitulo.co')) {
+                  storageData.forEach(item => {
+                      window.localStorage.setItem(item.name, item.value);
+                  });
                 }
             });
 
@@ -79,7 +72,6 @@ export class Configuration extends EventEmitter {
                 if (requestType === 'script' && route.request().frame() !== this.context.mainFrame()) {
                     return route.abort();
                 }
-
                 //block unnecessary resources to speed up loading times
                 if (['font', 'image', 'media', 'beacon'].includes(requestType)) {
                     route.abort()
@@ -87,7 +79,6 @@ export class Configuration extends EventEmitter {
                     route.continue()
                 }
             })
-
         } catch (e: any) {
             if (e.message.includes("Executable doesn't exist")) {
                 spin.fail(chalk.redBright('Error: El navegador Firefox Playwright no esta instalado'))
@@ -95,42 +86,42 @@ export class Configuration extends EventEmitter {
             }
         }
     }
-
+  
     async closeBrowser() {
         try {
             if(!this.browser) return
-            spin.start('Close browser...')
+            spin.start('Closing browser...')
             await this.context.close()
             await this.browser.close()
-            this.browser = null
             spin.stop()
+            this.browser = null
             this.emit('close')
         } catch (e) {
+            if(spin.isSpinning) spin.stop()
             console.log(e)
         }
     }
+    
     async load(conf: ConfigurationInterface | null = null) {
         try {
 
             if (fs.existsSync(CONFIG_FILE_PATH) && !conf) {
                 const confJSON = (await fsPromise.readFile(CONFIG_FILE_PATH)).toString()
-                this.config = await schema.parseAsync(JSON.parse(confJSON)) as ConfigurationInterface
+                this.config = await JSON.parse(confJSON) as ConfigurationInterface
             }
-            this.lang = lang[this.config.language]
-
+            if(this.config.language)
+              this.lang = lang[this.config.language]
             if (this.config.client.need_browser && !this.browser) {
                 await this.loadBrowser()
             }else if(!this.config.client.need_browser && this.browser){
                 await this.closeBrowser()
             }
-
+          /* */
             if (
                 (!this.context || !this.browser) &&
                 this.config.client.need_browser
-            ) throw new Error('Fatal init error')
-
+            ) throw new Error('Error on browser loading')
             this.client = (mangaServerRegister[this.config.client.name].client)(this.context)
-            await this.store()
             this.emit('load', this.config, this.client, this.context,this.browser)
         } catch (e) {
             console.log(e)
@@ -140,7 +131,7 @@ export class Configuration extends EventEmitter {
         try {
             await fsPromise.writeFile(
                 CONFIG_FILE_PATH,
-                JSON.stringify(schema.parse(this.config), null, '\t'))
+                JSON.stringify(this.config, null, '\t'))
         } catch (e) {
             console.log(e)
         }
@@ -154,10 +145,12 @@ export class Configuration extends EventEmitter {
         return null
     }
     getClient() {
-            return this.client 
+        return this.client 
     }
     async setConfig(conf: ConfigurationInterface) {
+        if(conf)
         await this.load(conf)
+        await this.store()
         this.emit('update', this.config, this.client, this.lang)
     }
 }
