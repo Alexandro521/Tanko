1599d560778e92704bc004cd786f3cd28d374f64
Alexandro521 <elavion0151@gmail.com>
2026-06-04 23:13:53 -0400

    Update history.ts

--- src/functions/history.ts
+++ src/functions/history.ts
@@ -1,7 +1,6 @@
 import type { HistoryObject } from "../types/types.js";
-import fs from 'node:fs'
-import fsp from 'node:fs/promises'
 import { HISTORY_PATH } from "../const.js";
+import fsp from 'node:fs/promises'
 
 interface HistoryDataStruct {
   last_update: number,
@@ -10,50 +9,49 @@ interface HistoryDataStruct {
 
 export class History {
   private static map = new Map<string, HistoryObject>()
-
-  static fetch(): HistoryObject[] {
+  static parseMap(): HistoryObject[] {
     return Array.from(this.map.values())
       .sort((a, b) => b.time - a.time)
   }
+      
   static async load() {
     try {
-      if (!fs.existsSync(HISTORY_PATH)) {
-        await fsp.writeFile(HISTORY_PATH, JSON.stringify({ last_update: Date.now(), History: [] }, null, '\t'))
-        return true
-      }
-      const history = await fsp.readFile(HISTORY_PATH, {encoding: 'utf-8'})
-      const obj = <HistoryDataStruct>JSON.parse(history.toString())
-      if (!obj.history) {
-        /*recovering corrupted file */
-        await fsp.writeFile(HISTORY_PATH, JSON.stringify({ last_update: Date.now(), History: [] }, null, '\t'))
-      }
-      obj.history.forEach(e => {
-        if (e && e.mangaTitle.length > 0) {
+      const historyRaw = await fsp.readFile(HISTORY_PATH, 'utf-8')
+      const historyObject = <HistoryDataStruct>JSON.parse(historyRaw)
+      historyObject.history.forEach(e => {
+        if (e.mangaTitle.length > 0) {
+          const date = new Date()
+          date.setTime(e.time)
+          console.log(date.toLocaleTimeString())
           this.map.set(e.mangaTitle, e);
         }
       })
       return true;
     } catch (e) {
+      if (e instanceof Error) {
+        console.log(e.message)
+        await fsp.writeFile(HISTORY_PATH, JSON.stringify({ last_update: Date.now(), history: [] }, null))
+      }
       return false
     }
   }
-  
   static save(mangaInfo: HistoryObject) {
     mangaInfo.time = Date.now()
     this.map.set(mangaInfo.mangaTitle, mangaInfo)
     this.store()
   }
-  
   static async store() {
     try {
       if (this.map.size < 1) return;
       const fileStruct: HistoryDataStruct = {
         last_update: Date.now(),
-        history: Array.from(this.map.values()).sort((a, b) => b.time - a.time)
+        history: this.parseMap()
       }
-      await fsp.writeFile(HISTORY_PATH, JSON.stringify(fileStruct, null, '/t'))
+      await fsp.writeFile(HISTORY_PATH, JSON.stringify(fileStruct, null))
     } catch (e) {
-      console.log(e)
+      if (e instanceof Error) {
+        console.error(e.message)
+      }
     }
   }
 }
