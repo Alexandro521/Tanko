9c8b64e934783156924d631a0ac7233a9200e073
Alexandro521 <elavion0151@gmail.com>
2026-06-08 23:10:15 -0400

    Making some adjustments, without major changes

--- src/functions/downloader.ts
+++ src/functions/downloader.ts
@@ -25,32 +25,32 @@ const PDFOptions: PDFKit.PDFDocumentOptions = {
 const cfgInst = await Configuration.getInstance()
 
 export async function downloadChapter(mangaTitle: string, chapterTitle: string, srcs: ChapterPage[]) {
-  try {
-        const {loading_states, err_messages} = cfgInst.getLang()
+    try {
+        const { loading_states, err_messages } = cfgInst.getLanguageInterface()
         spin.start(loading_states.downloading_pages + '...')
         const dir = makeDir('pdf', mangaTitle) ?? './';
         const pdf = new PDFDocument(PDFOptions)
         pdf.pipe(fs.createWriteStream(`${dir}/${sanitize(chapterTitle)}.pdf`));
         let progressCounter = 0;
-        let pageImages =  await Promise.all(srcs.map(async (image, index) => {
+        let pageImages = await Promise.all(srcs.map(async (image, index) => {
             if (image.src === 'undefined') throw new Error(err_messages.pdf_make.msg);
             const data = await fetch(image.src)
-            if(!data.ok) throw new Error('Error al descargar el capitulo')
-                //convert webp buffer to jpeg buffer
-            const buffer = await sharp(await data.arrayBuffer()).jpeg({quality: 100, optimiseCoding: true}).toBuffer({resolveWithObject:true});
+            if (!data.ok) throw new Error('Error al descargar el capitulo')
+            //convert webp buffer to jpeg buffer
+            const buffer = await sharp(await data.arrayBuffer()).jpeg({ quality: 100, optimiseCoding: true }).toBuffer({ resolveWithObject: true });
             spin.text = `${loading_states.downloading_pages} [${progressCounter}/${srcs.length}] page #${image.page_index}`
             progressCounter++;
-            return {index, buffer};
+            return { index, buffer };
         }))
         //sort buffer pages;
-        pageImages = pageImages.sort((a,b)=> a.index -  b.index);
+        pageImages = pageImages.sort((a, b) => a.index - b.index);
         progressCounter = 1;
-        for(let {buffer, index} of pageImages){
+        for (let { buffer, index } of pageImages) {
 
-            const {height, width} = buffer.info;
-            const page = pdf.addPage({size: [width, height],margin: '0%' })
-            page.image(buffer.data, {align: 'center',cover: [width,height] })
-            spin.text = `Making PDF ${(progressCounter*pageImages.length)/100}%... append image #${index}`
+            const { height, width } = buffer.info;
+            const page = pdf.addPage({ size: [width, height], margin: '0%' })
+            page.image(buffer.data, { align: 'center', cover: [width, height] })
+            spin.text = `Making PDF ${(progressCounter * pageImages.length) / 100}%... append image #${index}`
             progressCounter++;
         }
         pdf.end();
@@ -64,7 +64,7 @@ export async function downloadChapter(mangaTitle: string, chapterTitle: string, 
 
 export function makeDir(...name: string[]) {
     try {
-    
+
         const targetDir = path.join(DOWNLOADS_DEFAULT_DIR, ...(name.map((n) => sanitize(n).replaceAll(' ', '-'))))
         if (!fs.existsSync(targetDir)) {
             fs.mkdir(
@@ -72,7 +72,6 @@ export function makeDir(...name: string[]) {
                 { recursive: true, },
                 (err) => {
                     if (err) {
-
                         console.log(JSON.stringify(err));
                     }
                 }
