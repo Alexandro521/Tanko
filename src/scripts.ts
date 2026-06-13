5733ab3efc0e1cc6c75a4283ba241e134f475a00
Alexandro521 <elavion0151@gmail.com>
2026-06-12 23:33:37 -0400

    add: archivo para scripts, nuevo script para comprobar nuevas actualizaciones de la aplicacion

---
+++ src/scripts.ts
@@ -0,0 +1,38 @@
+import axios from "axios"
+import ansi from 'ansi-escapes'
+import pkgInfo from "../package.json" with {type: 'json'}
+import { Notify, NotifyType } from "./functions/notify.js"
+import chalk from "chalk"
+const notify = Notify.getInstace()
+
+
+export async function versionVerify(){
+    try{
+    const res = await axios.get('https://registry.npmjs.org/tanko')
+    const versions = Object.keys(res.data.versions)
+    const lastVersion = versions[versions.length -1]
+    if(pkgInfo.version !== lastVersion){
+        const message = `
+A new version of Tanko is available!
+from ${chalk.redBright(pkgInfo.version)} to ${chalk.greenBright(lastVersion)}
+Run: 
+${chalk.blueBright('$pnpm add -g tanko')} to update to the latest version and enjoy the new features.
+
+Go to ${ansi.link(chalk.underline.blueBright('releases'), 'https://github.com/Alexandro521/Tanko/releases')} to view the release notes`
+notify.push({
+    title: `Update avalible ${lastVersion}`,
+    type: NotifyType.message,
+    message
+})
+}
+return false
+    }catch(e){
+        if(e instanceof Error){
+            notify.push({
+                title: e.name,
+                type: NotifyType.error,
+                message: e.message
+            })
+        }
+    }
+}
