2697e4ad4a67cd5a5f14483c7a010cf48caecd48
Alexandro521 <elavion0151@gmail.com>
2026-06-12 23:35:02 -0400

    update: comprobar nuevas acutalizaciones al iniciar y comprobar si es la primera ejecucion

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ import ansi from 'ansi-escapes'
 import ansiEscapes from 'ansi-escapes'
 import { History } from "./functions/history.js";
 import { main } from "./cli/menu.js";
-import { BASE_DIR, BROWSER_STORAGE_PATH, DATA_DEFAULT_DIR, DOWNLOADS_DEFAULT_DIR, HISTORY_PATH, WELCOME_MESSAGE } from './const.js'
+import { BASE_DIR, BROWSER_STORAGE_PATH, DATA_DEFAULT_DIR, DOWNLOADS_DEFAULT_DIR, FIRST_INIT_MESSAGE, HISTORY_PATH, WELCOME_MESSAGE } from './const.js'
 import { Configuration } from './functions/configuration.js';
 import { Notify, NotifyType } from './functions/notify.js';
+import { versionVerify } from './scripts.js';
+
 console.log(ansiEscapes.clearTerminal)
 
 if(!fs.existsSync(BASE_DIR)) {
@@ -26,11 +28,24 @@ if (!fs.existsSync(BROWSER_STORAGE_PATH)) {
   await fsp.mkdir(BROWSER_STORAGE_PATH, {recursive: true})
 }
 
+
+
 console.log(ansi.clearViewport);
 console.log(WELCOME_MESSAGE);
 await History.load() 
-const confInstance = await Configuration.getInstance()
 const notify = Notify.getInstace()
+const confInstance = await Configuration.getInstance()
+
+if(confInstance.configuration.isFirstRun) {
+  notify.push({
+    title: 'Welcome!',
+    type: NotifyType.message,
+    message: FIRST_INIT_MESSAGE
+  })
+  confInstance.configuration.isFirstRun = false
+}
+await versionVerify()
+
 await main(confInstance)
 await confInstance.closeBrowser();
 await confInstance.writeConfigFile()
