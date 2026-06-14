95b953ad546c44ef0720d14c782bd5126a49badf
Alexandro521 <elavion0151@gmail.com>
2026-06-13 23:15:17 -0400

    implementando el nuevo contrato

--- src/servers/leerCapitulo.ts
+++ src/servers/leerCapitulo.ts
@@ -10,6 +10,7 @@ import type {
     SearchResult,
     MangaInfo
 } from "../types/types.js"
+import { json } from "stream/consumers";
 
 export class LeerCapitulo implements MangaProvider {
     private page: Page
@@ -28,35 +29,14 @@ export class LeerCapitulo implements MangaProvider {
             const container = $('div.mainpage-manga div.media-body', node)
             const mangaTtitle = container.find('h4.manga-newest').text()
             const src = container.find('a').first().attr('href') ?? ''
-            const chapterList: Chapter[] = []
-            container.find('div.hotup-list > span').each((_, span) => {
-                const chapterSrc = $('a.xanh', span)
-                const chapter: Chapter = {
-                    id: 'null',
-                    title: chapterSrc.text() ?? '',
-                    translation_count: 1,
-                    translations: {es: { lang: 'es', src: chapterSrc.attr('href') ?? 'null', title: chapterSrc.text()}
-                    }
-                }
-                chapterList.push(chapter)
-            })
-            mangaList.push({ title: mangaTtitle, src, chapters: sortChapterList(chapterList) })
+            mangaList.push({ title: mangaTtitle, src})
         })
         return mangaList
     }
+    async getChapterList(mangaSrc: string): Promise<Chapter[]> {
+        const res = await this.axios.get(this.baseUrl + mangaSrc)
 
-    async search(query: string): Promise<SearchResult[]> {
-        const res = await this.axios.get(`/search-autocomplete?term=${query}`,);
-        const json: SearchResult[] = JSON.parse(res.data)
-        return json
-    }
-    async getMangaInfo(mangaSrc: string): Promise<MangaInfo> {
-        const res = await fetch(this.baseUrl + mangaSrc)
-                if (!res.ok) {
-            throw new Error('red failed');
-        }
-        const $ = cheerio.load(await res.text())
-        const title = $('h1.title-manga').text()
+        const $ = cheerio.load(await res.data)
         const chapters: Chapter[] = []
         $('div.chapter-list ul > li').each((i, node) => {
             const anchor = $(node).find('a.xanh')
@@ -69,10 +49,27 @@ export class LeerCapitulo implements MangaProvider {
                 }
             })
         })
+        return sortChapterList(chapters)
+    }
+    async search(query: string): Promise<MangaInfo[]> {
+        const res = await this.axios.get(`/search-autocomplete?term=${query}`,);
+        const data:SearchResult[] = JSON.parse(res.data)
+        const mangaList = data.map((e):MangaInfo=>{
+            return {
+                src: e.link,
+                title: e.label ?? e.value,
+                description: e.value
+            }
+        })
+        return mangaList
+    }
+    async getMangaInfo(mangaSrc: string): Promise<MangaInfo> {
+        const res = await this.axios.get(this.baseUrl + mangaSrc)
+        const $ = cheerio.load(await res.data)
+        const title = $('h1.title-manga').text()
         return {
             title,
-            src: mangaSrc,
-            chapters: sortChapterList(chapters)
+            src: mangaSrc
         }
     }
     async getChapterPages(chapterSrc: string): Promise<ChapterPage[]> {
@@ -95,22 +92,12 @@ export class LeerCapitulo implements MangaProvider {
         const Populars: MangaInfo[] = []
         $('div.update-list div div > .hot-manga').each((index, e) => {
             const titleNode = $(e).find('div.caption a h3.manga-title')
-            const chapterNode = $(e).find('div.caption a.Chapter')
             const src = $(titleNode.parent()).attr('href') ?? 'null';
+            
             const title = titleNode.text()
-            const chapterTitle = chapterNode.find('p.chapter').text()
-            const chapterSrc = chapterNode.attr('href') ?? 'null'
-            const ch: Chapter = {
-                title: chapterTitle,
-                id: chapterSrc,
-                translation_count: 1,
-                translations: {es: {lang: 'es', title: chapterTitle, src: chapterSrc}}
-
-            }
             Populars.push({
                 title,
-                src,
-                chapters: [ch]
+                src
             })
         })
         return Populars
--- src/servers/mangadex.ts
+++ src/servers/mangadex.ts
@@ -10,63 +10,99 @@ import type {
     MangaInfo
 } from "../types/types.js";
 import axios from "axios";
-
-import type { SearchResultMangadex } from "../types/mangadex/search.js";
+import fs from 'fs/promises'
+import type { MangadexMangaInfo, SearchResultMangadex } from "../types/mangadex/search.js";
 import type { Puzzle } from "../types/mangadex/get_pages.js";
-
+interface Chapter_ extends Chapter {
+    index: number | string
+}
+enum MangaOrderEnum  {
+    asc = 'asc',
+    desc = 'desc'
+}
+interface ChapterOrderOptions {
+    createdAt?: MangaOrderEnum,
+    updatedAt?: MangaOrderEnum,
+    publishAt?: MangaOrderEnum,
+    readableAt?: MangaOrderEnum,
+    volume?: MangaOrderEnum,
+    chapter?: MangaOrderEnum,
+}
+interface MangaOrderOptions {
+    title?: MangaOrderEnum,
+    year?: MangaOrderEnum,
+    createdAt?: MangaOrderEnum,
+    updatedAt?: MangaOrderEnum,
+    latestUploadedChapter?: MangaOrderEnum,
+    followedCount?: MangaOrderEnum,
+    relevance?: MangaOrderEnum
+}
+interface GetProps {
+    order: MangaOrderOptions | ChapterOrderOptions,
+    maxGet: number,
+    chapterRequestStepSize: number,
+    maxChapterRequest: number
+}
 export class MangaDex implements MangaProvider {
     public name: ServerName = "mangadex"
     private baseUrl = 'https://api.mangadex.org'
-
-    async search(query: string): Promise<SearchResult[]> {
+    private chapterOrder: Object;
+    constructor(){
+        this.chapterOrder = this.serializeOrderObject({
+            chapter: MangaOrderEnum.desc,
+        })
+        
+    }
+    
+    async search(query: string): Promise<MangaInfo[]> {
 
         const { data } = await axios.get<SearchResultMangadex>(`${this.baseUrl}/manga`, {
             params: {
                 "title": query,
             }
         })
-
-        return data.data.map((e): SearchResult => {
+        return data.data.map((e): MangaInfo => {
             return {
-                thumbnail: "",
-                label: e.attributes.title.en ?? e.attributes.title["ja-ro"] ?? '',
-                link: e.id,
-                value: e.attributes.title.en ?? ''
+                title: e.attributes.title.en ?? Object.values(e.attributes.title)[0],
+                src: e.id,
+                description: e.attributes.description.en ?? Object.values(e.attributes.description)[0]
             }
         })
     }
-
-    async getMangaInfo(mangaSrc: string): Promise<MangaInfo> {
-
-        let offset = 0
-        let data = [];
+    async getChapterList(mangaSrc: string, chapterRequestSize = 300, chapterReqLimit=9999): Promise<Chapter[]> {
+        let chapterOffset = 0
+        let chapters:ChapterListMangadex['data'] = [];
+        const params = {
+            'limit': chapterRequestSize,
+            'offset': chapterOffset,
+            'includeExternalUrl': 0,
+            'includeEmptyPages': 0,
+            'includeFuturePublishAt': 0,
+            ...this.chapterOrder
+        }
         while (true) {
             const res = await axios.get<ChapterListMangadex>(
-                `${this.baseUrl}/manga/${mangaSrc}/feed?limit=300&offset=${offset}&includeExternalUrl=0`
-            );
-            data.push(...res.data.data);
-            if (data.length >= res.data.total)
+                `${this.baseUrl}/manga/${mangaSrc}/feed`, {params});
+            chapters.push(...res.data.data);
+            const chapterLimit = Math.min(res.data.total, chapterReqLimit)
+            if (chapters.length >=  chapterLimit)
                 break;
-            offset += Math.min(300, res.data.total - data.length);
+            chapterOffset += Math.min(chapterRequestSize, chapterLimit - chapters.length);
         }
-
         const chapterMap = new Map<string, number>()
-        interface Chapter_ extends Chapter { index: number | string }
-
         const chapterList: Chapter_[] = []
-
-        for (const chapterData of data) {
+        /**Adjuntar todas las variantes de idioma en una sola unidad */
+        for (const chapterData of chapters) {
             const chapterNumber = chapterData.attributes.chapter ?? ''
             const chapterLang = chapterData.attributes.translatedLanguage as ChapterLangType
             const title =
                 `Chapter ${chapterData.attributes.chapter}` +
                 `${chapterData.attributes.title != null ? ': ' + chapterData.attributes.title : ''}`
-
+    
             if (chapterMap.has(chapterNumber)) {
-
+    
                 const target = chapterList[chapterMap.get(chapterNumber) ?? 0];
                 target.translation_count++
-
                 target.translations[chapterLang] = {
                     title,
                     lang: chapterLang,
@@ -74,7 +110,7 @@ export class MangaDex implements MangaProvider {
                 }
                 continue
             }
-
+    
             const chapterInfo: Chapter_ = {
                 title,
                 translation_count: 1,
@@ -82,7 +118,7 @@ export class MangaDex implements MangaProvider {
                 id: chapterData.id,
                 index: chapterNumber,
             }
-
+    
             chapterInfo.translations[chapterLang] = {
                 title,
                 lang: chapterLang,
@@ -92,17 +128,19 @@ export class MangaDex implements MangaProvider {
             chapterMap.set(chapterNumber, chapterList.length - 1)
         }
 
-        chapterList.sort((a, b) => Number(b.index) - Number(a.index))
+        return chapterList
+    }
+    async getMangaInfo(mangaSrc: string): Promise<MangaInfo> {
+        const {data} =  await axios.get<MangadexMangaInfo>(`${this.baseUrl}/manga/${mangaSrc}`)
+        const {attributes, id} = data.data
         return {
-            title: chapterList[0]?.title ?? '',
-            src: mangaSrc,
-            chapters: chapterList
+            title: Object.values(attributes.title)[0] ?? 'No title',
+            src: id,
+            description: Object.values(attributes.description)[0] ?? 'No title',
+            lastUploadChapterSrc: attributes.latestUploadedChapter
         }
     }
-
     async getChapterPages(chapterSrc: string): Promise<ChapterPage[]> {
-
-        /*Get pages URL*/
         const res = await axios.get<Puzzle>(`${this.baseUrl}/at-home/server/${chapterSrc}`)
         const { baseUrl, chapter } = res.data
         const pages = chapter.data.map((e, i): ChapterPage => {
@@ -113,11 +151,56 @@ export class MangaDex implements MangaProvider {
         })
         return pages
     }
-    getPopulars(): Promise<MangaInfo[]> {
-        throw new Error("Method not implemented.");
+    private serializeOrderObject(order: MangaOrderOptions | ChapterOrderOptions) {
+        const finalOrderQuery: {[key: string]: MangaOrderEnum} = {};
+        // { "order[rating]": "desc", "order[followedCount]": "desc" }
+        for (const [key, value] of Object.entries(order)) {
+        finalOrderQuery[`order[${key}]`] = value;
+        }
+        return finalOrderQuery
     }
-    getLastMangas(): Promise<MangaInfo[]> {
-        throw new Error("Method not implemented.");
+    private async generalizeMangaGet(props:GetProps): Promise<MangaInfo[]>{
+        const orderQuery = this.serializeOrderObject(props.order)
+        const res = await axios.get<SearchResultMangadex>(`${this.baseUrl}/manga`,{
+            params: {
+                'limit': props.maxGet,
+                'offset': 0,
+                ...orderQuery
+            }
+        })
+        const mangaList = res.data.data.map((e):MangaInfo   =>{
+            const {attributes} = e
+            return {
+                title: Object.values(attributes.title)?.[0] ?? 'no title',
+                src: e.id,
+                description: Object.values(attributes.description)?.[0] ?? 'no description',
+                lastUploadChapterSrc: attributes.latestUploadedChapter
+            }
+        })
+        return mangaList
     }
-
+    async getPopulars(): Promise<MangaInfo[]> {
+        const order:MangaOrderOptions = {
+            followedCount: MangaOrderEnum.desc,
+            relevance: MangaOrderEnum.desc
+        }
+        return await this.generalizeMangaGet({
+            order,
+            chapterRequestStepSize: 1,
+            maxChapterRequest: 1,
+            maxGet: 60
+        })
+    }
+    async getLastMangas(): Promise<MangaInfo[]> {
+        const order:MangaOrderOptions = {
+            latestUploadedChapter: MangaOrderEnum.desc,
+            updatedAt: MangaOrderEnum.desc,
+        }
+        return await this.generalizeMangaGet({
+            order,
+            chapterRequestStepSize: 10,
+            maxChapterRequest: 10,
+            maxGet: 60
+        })
+    }
 }
