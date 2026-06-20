04ef9fb63eb8ca7ddf1ba4b32a4414b4adc721b6
Alexandro521 <elavion0151@gmail.com>
2026-06-19 23:28:05 -0400

    New download class to support multiple formats

--- src/functions/downloader.ts
+++ src/functions/downloader.ts
@@ -1,17 +1,20 @@
 import fs from 'fs'
-import path from 'path';
 import sharp from 'sharp';
-import sanitize from 'sanitize-filename';
 import ora from 'ora';
 import chalk from 'chalk';
+import path from 'path';
+import sanitize from 'sanitize-filename';
 import ansi from 'ansi-escapes'
 import PDFDocument from 'pdfkit'
-import type { ChapterPage } from '../types/types.js';
 import { DOWNLOADS_DEFAULT_DIR } from '../const.js';
-import { Configuration } from './configuration.js';
+import { Configuration, ConfigurationEvents } from './configuration.js';
 import { Notify, NotifyType } from './notify.js';
-const spin = ora();
+import { makeDir } from '../utils.js';
+import { EventEmitter } from 'events';
+import { DownloadFormat } from '../types/enum.js';
+import type { ChapterPage, DownloadProps, FormatProps, ImgBuffer } from '../types/types.js';
 
+const spin = ora();
 const PDFOptions: PDFKit.PDFDocumentOptions = {
     margin: '0',
     compress: true,
@@ -25,72 +28,94 @@ const PDFOptions: PDFKit.PDFDocumentOptions = {
     }
 }
 const noti = Notify.getInstace()
-const cfgInst = await Configuration.getInstance()
+const config = await Configuration.getInstance()
+let { loading_states, err_messages } = await config.getLanguageInterface()
+config.on(ConfigurationEvents.updateLanguage, async ()=>{
+    const lang = await config.getLanguageInterface()
+    loading_states = lang.loading_states
+    err_messages = lang.err_messages
+})
 
-export async function downloadChapter(mangaTitle: string, chapterTitle: string, srcs: ChapterPage[]) {
-    try {
-        const { loading_states, err_messages } = await cfgInst.getLanguageInterface()
-        spin.start(loading_states.downloading_pages + '...')
-        const downloadPath = makeDir('pdf', mangaTitle) ?? './';
-        const pdf = new PDFDocument(PDFOptions)
-        pdf.pipe(fs.createWriteStream(`${downloadPath}/${sanitize(chapterTitle)}.pdf`));
-        let progressCounter = 0;
-        let pageImages = await Promise.all(srcs.map(async (image, index) => {
-            if (image.src === 'undefined') throw new Error(err_messages.pdf_make.msg);
-            const data = await fetch(image.src)
-            if (!data.ok) throw new Error('Error al descargar el capitulo')
-            //convert webp buffer to jpeg buffer
-            const buffer = await sharp(await data.arrayBuffer()).jpeg({ quality: 100, optimiseCoding: true }).toBuffer({ resolveWithObject: true });
-            spin.text = `${loading_states.downloading_pages} [${progressCounter}/${srcs.length}] page #${image.page_index}`
-            progressCounter++;
-            return { index, buffer };
-        }))
-        //sort buffer pages;
-        pageImages = pageImages.sort((a, b) => a.index - b.index);
-        progressCounter = 1;
-        for (let { buffer, index } of pageImages) {
-
-            const { height, width } = buffer.info;
-            const page = pdf.addPage({ size: [width, height], margin: '0%' })
-            page.image(buffer.data, { align: 'center', cover: [width, height] })
-            spin.text = `Making PDF ${(progressCounter * pageImages.length) / 100}%... append image #${index}`
-            progressCounter++;
-        }
-        pdf.end();
-        spin.stop();
-        noti.push({
-            title: 'Download complete',
-            type: NotifyType.event,
-            message: `${mangaTitle} chapter, ${ansi.link(chalk.underline.blueBright(chapterTitle) ,downloadPath)} downloaded`
-        })
-    } catch (e) {
-        if(e instanceof Error){
-            noti.push({
-            title: 'Download failed',
-            type: NotifyType.error,
-            message: `${e.message}`
-        })
-        }
+export class Downloader extends EventEmitter{
+    private static instance: Downloader;
+    private constructor(){
+        super()
     }
-}
-
-export function makeDir(...name: string[]) {
-    try {
-
-        const targetDir = path.join(DOWNLOADS_DEFAULT_DIR, ...(name.map((n) => sanitize(n).replaceAll(' ', '-'))))
-        if (!fs.existsSync(targetDir)) {
-            fs.mkdir(
-                targetDir,
-                { recursive: true, },
-                (err) => {
-                    if (err) {
-                        console.log(JSON.stringify(err));
-                    }
-                }
-            )
+    private async getImagesBuffer(images: ChapterPage[]) {
+        const contentRexp = new RegExp(/image\/(webp|jpeg|png)/)
+        const fetchImage = async (img: ChapterPage, index: number): Promise<{index: number, data: ImgBuffer}> => {
+            return new Promise(async (resolve, reject) => {
+                const res = await fetch(img.src)
+                const contentType = res.headers.get('Content-Type') ?? ''
+                if (!res.ok) reject(err_messages.fetching.msg)
+                else if (!contentRexp.test(contentType)) reject(`Invalid mime type ${contentType}`)
+                let buffImg: ImgBuffer = 
+                await sharp((await res.arrayBuffer()))
+                    .jpeg()
+                    .toBuffer({ resolveWithObject: true });
+                
+                this.emit('download_page',index)
+                resolve({ index, data: buffImg })
+            })
         }
-        return targetDir
-    } catch (e) {
-        console.log('error')
+        const pagesBuffer = await Promise.all(images.map(fetchImage))
+        return pagesBuffer
+    }
+    private async pdf(props: FormatProps){
+        let images = await this.getImagesBuffer(props.pages);
+        this.emit('onpdf')
+        const doc = new PDFDocument(PDFOptions)
+        doc.pipe(fs.createWriteStream(`${props.path}.pdf`))
+        images = images.sort((a, b) => a.index - b.index);
+        for (const { data } of images) {
+            const { height, width } = data.info;
+            const page = doc.addPage({ size: [width, height], margin: '0%' })
+            page.image(data.data, { align: 'center', cover: [width, height] })
+        }
+        this.emit('done')
+        doc.end();
+    }
+    private async images(props: FormatProps) {
+
+    }
+    private async cbz(props: FormatProps) {
+
+    }
+    private async zip(props: FormatProps) {
+
+    }
+    static getInstance(){
+        if(!this.instance){
+            this.instance = new Downloader();
+        }
+        return this.instance
+    }
+    async download(props: DownloadProps) {
+            const downloadPath = await makeDir(
+                DOWNLOADS_DEFAULT_DIR,
+                props.format,
+                props.serverName,
+                props.mangaTitle,
+            )
+            const fprops: FormatProps = {
+                pages: props.pages,
+                path: path.join(downloadPath, sanitize(props.chapterTitle))
+            }
+            switch(props.format){
+                case DownloadFormat.pdf:
+                    await this.pdf(fprops)
+                case DownloadFormat.cbz:
+                    await this.cbz(fprops)
+                case DownloadFormat.zip: 
+                    await this.zip(fprops)
+                case DownloadFormat.img: 
+                    await this.images(fprops)
+            }
+            noti.push({
+                title: 'Download complete',
+                type: NotifyType.event,
+                message:
+                    `${props.mangaTitle} chapter, ${ansi.link(chalk.underline.blueBright(props.chapterTitle), downloadPath)} downloaded`
+            })
     }
 }
