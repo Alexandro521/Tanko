7853396605345f3a5e752b14e4a2dff27e167c31
Alexandro521 <elavion0151@gmail.com>
2026-07-31 00:38:12 -0400

    Update: Refactoring and implementing new methods for terminal management

--- src/functions/reader.ts
+++ src/functions/reader.ts
@@ -1,66 +1,73 @@
-import ora from "ora";
-import type { ChapterPage, Chapter, MangaProvider, Translations, ChapterLanguage } from "../types/types.js";
-import { Configuration } from "./configuration.js";
-import { ImageLoader } from "./images.js"
-import { History } from "./history.js";
-import readLine from 'readline'
-import esc from 'ansi-escapes'
-import { Notify } from "./notify.js";
-
-const LOADER = ora()
+import type { ChapterPage, Chapter, MangaProvider, Translations, ChapterLanguage, WSZ, TermImgProtocolName, TankoTermImgOutput, LoadImageProps } from "../types/types.js";
+import { Configuration } from "./configuration.ts";
+import { ImageLoader } from "./images.ts"
+import { History } from "./history.ts";
+import { Notify } from "./notify.ts";
+import ansi from 'ansi-escapes'
 let CONFIGURATION = await Configuration.getInstance()
-let { err_messages, loading_states, reader } = await CONFIGURATION.getLanguageInterface()
 
 export class PagesControl {
     private pages!: ChapterPage[];
+    private currenPage!:ChapterPage
     private readCheckList!: boolean[]
     private pageIndex = 0;
-    private imageLoader = new ImageLoader()
+    imageLoader = new ImageLoader()
     constructor(pages: ChapterPage[]) {
         this.pages = pages;
+        this.currenPage = pages[0]
         this.readCheckList = new Array(pages.length).fill(false)
     }
     nextPage() {
         if (this.pageIndex < this.pages.length - 1)
             this.pageIndex++;
+        this.currenPage = this.pages[this.pageIndex]
     }
     backPage() {
         if (this.pageIndex > 0)
             this.pageIndex--;
+        this.currenPage = this.pages[this.pageIndex]
     }
     reset() {
         this.pageIndex = 0;
+        this.currenPage = this.pages[0]
         this.imageLoader.free()
     }
-    async loadPage() {
+    cacheHit(page: ChapterPage){
+        return this.imageLoader.cacheHit(page)
+    }
+    async loadPage(props: LoadImageProps) {
         try {
-            if(!LOADER.isSpinning)
-                LOADER.start(loading_states.default_loading)
-            const image = await this.imageLoader.loadImage(this.pages[this.pageIndex])
-            if(LOADER.isSpinning)
-                LOADER.stop()
+            await this.imageLoader.loadImage(this.currenPage.src, props)
             if (!this.readCheckList[this.pageIndex]) {
                 this.readCheckList[this.pageIndex] = true
-            }
-            process.stdout.write(image)
+            } 
         } catch (e) {
-            LOADER.fail(err_messages.page_loading.msg)
             if(e instanceof Error)
                 Notify.pushError(e)
+        }
+    }
+    render() {
+        let page = this.currenPage
+        if(!page?.src) return
+        if (this.imageLoader.has(page.src)) {
+            const image = this.imageLoader.get(page.src) as TankoTermImgOutput
+            const pos = image.position
+            process.stdout.write(ansi.cursorTo(pos.x, pos.y))
+            process.stdout.write(ansi.cursorShow + image.data.encodedImg + ansi.cursorHide)
         }
     }
     getPages() {
         return this.pages
     }
-    getReadProgress(){
-        return ((this.readCheckList.filter((e)=> e === true)).length)/this.pages.length
+    get readProgress(){
+        return ((this.readCheckList.filter((e)=> e === true)).length * 100)/this.pages.length
     }
     setPages(newPages: ChapterPage[]) {
         this.pages = newPages;
         this.readCheckList = new Array(newPages.length).fill(false)
         this.reset()
     }
-    getIndex(){
+    get index(){
         return this.pageIndex
     }
     setIndex(newIndex: number) {
@@ -72,24 +79,88 @@ export class PagesControl {
 }
 
 export class TerminalControl {
-    static exitRawMode(keyHandler: (...arg: any[])=>void) {
-        process.stdout.write(esc.cursorShow)
+    static isRaw = false;
+    static graphicalProtocol: TermImgProtocolName ='kitty'
+    static wsz: WSZ
+
+    static exitRawMode(keyHandler: ((...arg: any[])=>void) | undefined = undefined ) {
+        if(!TerminalControl.isRaw) return
         if (typeof keyHandler === 'function')
             process.stdin.removeListener('keypress', keyHandler);
         process.stdin.setRawMode(false);
         process.stdin.pause();
-        process.stdout.write(esc.clearViewport);
+        TerminalControl.isRaw = false
     }
     static openRawMode(keyHandler: ((...arg: any[])=>void) | undefined = undefined) {
-        readLine.emitKeypressEvents(process.stdin)
-        process.stdin.resume()
+        if(TerminalControl.isRaw) return
         process.stdin.setRawMode(true)
+        process.stdin.resume()
         process.stdin.setEncoding('utf8');
-        process.stdout.write(esc.cursorHide)
+        TerminalControl.isRaw = true
         if (typeof keyHandler === 'function') {
             process.stdin.on('keypress', keyHandler)
         }
     }
+    static async request(request: string): Promise<string | undefined> {
+        return new Promise((resolve, reject) => {
+            const isRaw = TerminalControl.isRaw
+            let timer: null | NodeJS.Timeout = null
+            if (!isRaw) TerminalControl.openRawMode()
+
+            const recover = () => {
+                process.stdin.removeListener('data', stdinHandle)
+                if (!isRaw) TerminalControl.exitRawMode()
+                reject(undefined)
+            }
+
+            const stdinHandle = (data: string) => {
+                if (timer) clearTimeout(timer)
+                process.stdin.removeListener('data', stdinHandle)
+                if (!isRaw) TerminalControl.exitRawMode()
+                resolve(data)
+            }
+
+            process.stdin.on('data', stdinHandle)
+            timer = setTimeout(recover, 200)
+            process.stdout.write(request)
+        })
+    }
+    static async getWindowDimension(): Promise<WSZ | undefined> {
+        try{
+            let retrieves = 5
+            let res: string = ''
+            const regExp = /\x1b\[4;(\d+);(\d+)t/
+            while(retrieves > 0){
+                const str = await this.request('\x1B[14t')
+                if (str && RegExp(regExp).test(str)) {
+                    res = str
+                    break
+                }else {
+                    retrieves--;
+                    continue
+                }
+            }
+            const match = res.match(regExp);
+            if (match) {
+                const cellsDimension = process.stdout.getWindowSize()
+                const [height,width] = [parseInt(match[1], 10), parseInt(match[2], 10)]
+                this.wsz = ({
+                    w_height: height,
+                    w_width: width,
+                    w_colums: cellsDimension[0],
+                    w_rows: cellsDimension[1],
+                    w_cellPxWidth: width / cellsDimension[0],
+                    w_cellPxHeight: height / cellsDimension[1],
+                    w_ratio: width / height,
+                    w_position_x: 0,
+                    w_position_y: 0
+                });
+            return this.wsz
+        }
+        }catch(e){
+            return undefined
+        }
+    };
 }
 
 export class ChapterControl {
@@ -97,7 +168,7 @@ export class ChapterControl {
     private index!: number;
     private server!: MangaProvider;
     private lang!: Translations;
-    public isRead = false
+    public hasBeenTracked = false
 
     constructor(chapterList: Chapter[], chapterIndex: number, lang: Translations, server: MangaProvider) {
         this.chapters = chapterList
@@ -137,7 +208,7 @@ export class ChapterControl {
                 throw new Error("chapters out!");
             let target = this.extractChapterSrcByLang(this.chapters[this.index], this.lang);
             const data = await this.server.getChapterPages(target.id);
-            this.isRead = false
+            this.hasBeenTracked = false
             return data;
         } catch (e) {
             if(e instanceof Error){
@@ -149,18 +220,14 @@ export class ChapterControl {
         return this.lang
     }
     async prevChapter() {
-        if (this.index < this.chapters.length) {
+        if (this.index < this.chapters.length -1) {
             this.index++;
-            return true
         }
-        return false
     }
     async nextChapter() {
         if (this.index > 0) {
             this.index--;
-            return true
         }
-        return false
     }
     setChapterLanguage(newLang: Translations) {
         this.lang = newLang;
@@ -171,6 +238,14 @@ export class ChapterControl {
     }
     geChapterIndex() {
         return this.index
+    }
+    isFirstOrLast(){
+        /**
+         * 0 = none
+         * 1 = the first
+         * -1  =the last
+         */
+        return this.index === 0 ? -1 : this.index === this.chapters.length  -1 ? 1 : 0
     }
     historySave(title: string, src: string) {
         const chapter = this.extractChapterSrcByLang(this.getChapter(), this.lang)
