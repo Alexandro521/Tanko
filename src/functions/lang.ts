c27b5dc12cb6bdff3c5bdfd5595a44b1a321a94f
Alexandro521 <elavion0151@gmail.com>
2026-09-04 22:12:46 -0400

    Performance: Lazy loading for interface languages

--- src/functions/lang.ts
+++ src/functions/lang.ts
@@ -1,8 +1,5 @@
-import es from "../lang/es.json" with {type : "json"};
-import en from "../lang/en.json" with {type : "json"};
-import fr from "../lang/fr.json" with {type : "json"};
 
-export type LanguageInterface = typeof es
+export type LanguageInterface = Awaited<ReturnType<typeof LANGUAGE_REGISTER['en']>>
 export type LanguageErrorMessages = LanguageInterface['err_messages']
 export type LanguageAccessOptions = LanguageInterface['chapter_access_options']
 export type LanguageConfiguaration = LanguageInterface['configuration']
@@ -10,10 +7,11 @@ export type LanguageMainSections = LanguageInterface['main_sections']
 export type LanguageLoadingStates = LanguageInterface['loading_states']
 export type LanguageReader = LanguageInterface['reader']
 export type LangIso = keyof LanguageConfiguaration['lang_iso']
+export type AvalibleLanguageInterface = keyof typeof LANGUAGE_REGISTER
 
-export type AvalibleLanguageInterface = 'es' | 'en' | 'fr'
-export const LANGUAGE_REGISTER: {[key in AvalibleLanguageInterface]: typeof es} = {
-    es,
-    en,
-    fr
-} as const;
+export const LANGUAGE_REGISTER = {
+    es: async ()=> ( await import('../lang/es.json', {with: {type: "json"}})).default,
+    fr: async ()=> ( await import('../lang/fr.json', {with: {type: "json"}})).default,
+    en: async ()=> ( await import('../lang/en.json', {with: {type: "json"}})).default
+};
+
