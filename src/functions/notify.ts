9c47f30d372a0473274428117b66cf3c7ac01719
Alexandro521 <elavion0151@gmail.com>
2026-09-14 23:51:24 -0400

    update: removing object instance checks in the function body

--- src/functions/notify.ts
+++ src/functions/notify.ts
@@ -147,14 +147,12 @@ export class Notify extends EventEmitter{
     }
     static pushError(err: Error) {
         const notify = Notify.getInstace()
-        if (err instanceof Error) {
-            const props: NotifyProps = {
-                type: NotifyType.error,
-                message: err.message,
-                title: err.name,
-            }
-            notify.push(props)
+        const props: NotifyProps = {
+            type: NotifyType.error,
+            message: err.message,
+            title: err.name,
         }
+        notify.push(props)
     }
     static pushMessage(message: string, title = '') {
         const notify = Notify.getInstace()
