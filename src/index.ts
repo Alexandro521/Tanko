4761e2a61ffad88d7118605db7e826f7791897f7
Alexandro521 <elavion0151@gmail.com>
2026-09-14 23:46:12 -0400

    Refactoring: Replacing `fs.existsSync` with `fs.access` to avoid blocking operations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import fs from 'fs'
 import fsp from 'fs/promises'
 import ansi from 'ansi-escapes'
 import { main } from "./cli/menu.ts";
@@ -9,7 +8,6 @@ import {
   DATA_DEFAULT_DIR,
   DOWNLOADS_DEFAULT_DIR,
   FIRST_INIT_MESSAGE,
-  HISTORY_PATH,
   WELCOME_MESSAGE
 } from './const.ts'
 import { Configuration } from './functions/configuration.ts';
@@ -25,20 +23,39 @@ stdout.write(ansi.enterAlternativeScreen);
 
 loader.start('starting...')
 versionChecker()
-await Promise.all(
-  [
-    !fs.existsSync(BASE_DIR) ? 
-      fsp.mkdir(BASE_DIR, { recursive: true }) : undefined,
-    !fs.existsSync(DOWNLOADS_DEFAULT_DIR) ? 
-      fsp.mkdir(DOWNLOADS_DEFAULT_DIR, { recursive: true }) : undefined,
-    !fs.existsSync(DATA_DEFAULT_DIR) ?  
-      fsp.mkdir(DATA_DEFAULT_DIR, { recursive: true }) : undefined,
-    !fs.existsSync(HISTORY_PATH) ? 
-      fsp.writeFile(HISTORY_PATH, JSON.stringify({ last_update: Date.now(), history: [] })) : undefined,
-    !fs.existsSync(BROWSER_STORAGE_PATH) ? 
-      fsp.mkdir(BROWSER_STORAGE_PATH, { recursive: true }) : undefined,
+
+fsp.access(BASE_DIR)
+.catch(()=>{
+  fsp.mkdir(BASE_DIR, {recursive: true})
+})
+
+async function createPathIfNotExists(path: string){
+  return new Promise(async (resolve)=>{
+    try{
+      await fsp.access(path)
+      resolve(path)
+    }catch(e){
+      await fsp.mkdir(path, {recursive: true})
+      resolve(path)
+    }
+  })
+}
+
+try {
+  await Promise.all(
+    [
+    createPathIfNotExists(BASE_DIR),
+    createPathIfNotExists(DOWNLOADS_DEFAULT_DIR),
+    createPathIfNotExists(DATA_DEFAULT_DIR),
+    createPathIfNotExists(BROWSER_STORAGE_PATH)
   ]
 )
+}catch(e){
+  if(e instanceof Error){
+    Notify.pushError(e)
+  }
+}
+
 const notify = Notify.getInstace()
 const confInstance = await Configuration.getInstance()
 
@@ -53,7 +70,7 @@ if (confInstance.settings.tanko_isFirstRun) {
 
 loader.stop()
 stdout.write(WELCOME_MESSAGE);
-await main(confInstance)
+await main()
 await confInstance.conf_browser.close()
 await confInstance.store()
 stdout.write(ansi.exitAlternativeScreen);
